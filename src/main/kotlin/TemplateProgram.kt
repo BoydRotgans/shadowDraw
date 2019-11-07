ba572c6cb15d160d1d314be546ee5d00de5ff772
boydrotgans <boyd@lustlab.net>
2019-11-07 09:41:35 +0100

    init

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -3,28 +3,26 @@ import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.FontImageMap
 import org.openrndr.draw.loadImage
 import org.openrndr.draw.tint
+import org.openrndr.ffmpeg.FFMPEGVideoPlayer
+import org.openrndr.ffmpeg.VideoPlayerFFMPEG
 
 fun main() = application {
     configure {
-        width = 768
-        height = 576
+        width = 1280
+        height = 720
     }
 
     program {
-
-        val image = loadImage("file:data/images/pm5544.png")
-        val font = FontImageMap.fromUrl("file:data/fonts/IBMPlexMono-Regular.ttf", 64.0)
+        lateinit var videoPlayer: FFMPEGVideoPlayer
+        videoPlayer = FFMPEGVideoPlayer.fromURL("file:data/videos/demo.mp4")
+        videoPlayer.start()
+        var frame = 0
 
         extend {
-            drawer.drawStyle.colorMatrix = tint(ColorRGBa.WHITE.shade(0.2))
-            drawer.image(image)
-
-            drawer.fill = ColorRGBa.PINK
-            drawer.circle(Math.cos(seconds)*width/2.0+width/2.0, Math.sin(0.5*seconds)*height/2.0 + height/2.0, 140.0)
-
-            drawer.fontMap = font
-            drawer.fill = ColorRGBa.WHITE
-            drawer.text("OPENRNDR", width/2.0, height /2.0)
+            println(frame)
+            videoPlayer.next()
+            videoPlayer.draw(drawer)
+            frame++
         }
     }
 }
